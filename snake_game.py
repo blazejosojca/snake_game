b1bb9106c40df305eef6eb400a51adeab5981481
blazejosojca <blazejosojca@gmail.com>
2018-03-21 14:24:07 +0100

    first commit

---
+++ snake_game.py
@@ -0,0 +1,17 @@
+# Simply snake game based on pygame
+
+
+import pygame
+import sys
+import random
+import time
+
+
+check_errors = pygame.init()
+if check_errors[1] > 0:
+    print("(!) Had {0} initializing errors, exiting...".format(check_errors[1]))
+    sys.exit(-1)
+else:
+    print("(+) Pygame successfully initialized!")
+
+
