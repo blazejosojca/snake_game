8d6544a71692cd33a9fc4bdd44a68964b6abc033
blazejosojca <blazejosojca@gmail.com>
2018-03-22 00:08:09 +0100

    added some logic to game

--- snake_game.py
+++ snake_game.py
@@ -18,13 +18,13 @@ else:
 play_ground = pygame.display.set_mode((720, 560))
 pygame.display.set_caption('Snake game')
 
-# Color of playgroung
+# Color of playground
 
 red = pygame.Color(255, 0, 0) # gameover
-green = pygame.color.Color(0, 255, 0) # colour of snake
-black = pygame.color.Color(0, 0, 0) # score
-white = pygame.color.Color(255, 255, 255) # background
-brown = pygame.color.Color(255, 255, 50) # food
+green = pygame.Color(0, 255, 0) # colour of snake
+black = pygame.Color(0, 0, 0) # score
+white = pygame.Color(255, 255, 255) # background
+brown = pygame.Color(255, 255, 50) # food
 
 # time controller by frame per seconds
 fps_controller = pygame.time.Clock()
@@ -70,5 +70,45 @@ while True:
             elif event.key == pygame.K_ESCAPE:
                 pygame.event.post(pygame.event.Event(QUIT))
 
+    # validation of direction
+    if change_to == 'RIGHT' and not direction == 'LEFT':
+        direction = 'RIGHT'
+    if change_to == 'LEFT' and not direction == 'RIGHT':
+        direction = 'LEFT'
+    if change_to == 'UP' and not direction == 'RIGHT':
+        direction = 'UP'
+    if change_to == 'DOWN' and not direction == 'UP':
+        direction = 'DOWN'
+
+    # Change snake position
+    if direction == 'RIGHT':
+        snake_position[0] += 10
+    if direction == 'LEFT':
+        snake_position[0] -= 10
+    if direction == 'UP':
+        snake_position[1] += 10
+    if direction == 'DOWN':
+        snake_position[1] -= 10
+
+    # Snake body mechanism
+    snake_body.insert(0, list(snake_position))
+    if snake_position[0] == food_position[0] and snake_position[1] == food_position[1]:
+        food_spawn = False
+    else:
+        snake_body.pop()
+
+    # Food spawn
+    if food_spawn == False:
+        food_position = [random.randrange(1, 72) * 10, random.randrange(1, 56) * 10]
+    food_spawn = True
+
+    # Change color background
+    play_ground.fill(white)
+    for pos in snake_body:
+        pygame.draw.rect(play_ground, green, pygame.Rect(pos[0], pos[1], 10, 10))
+
+    pygame.draw.rect(play_ground, brown, pygame.Rect(food_position[0], food_position[1], 10, 10 ))
 
 
+    pygame.display.flip()
+    fps_controller.tick(25)
